4a4d72ac95e61ffd5690aa68c4cc333cb056f4cb
Med Diyae GHBALOU <meddiyae.ghbalou@gmail.com>
2024-06-04 14:40:49 +0100

    app complet

--- lib/Models/expression.dart
+++ lib/Models/expression.dart
@@ -19,12 +19,5 @@ class Expression {
       example: json['example'],
     );
   }
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['expressionName'] = expressionName;
-    data['image'] = image;
-    data['definition'] = definition;
-    data['example'] = example;
-    return data;
-  }
+  
 }
--- lib/Views/home_page.dart
+++ lib/Views/home_page.dart
@@ -1,11 +1,13 @@
+import 'dart:convert';
+
 import 'package:dictionary_app/Models/expression.dart';
-import 'package:dictionary_app/Models/list_expression.dart';
 import 'package:dictionary_app/utils/constants.dart';
 import 'package:dictionary_app/widgets/button_search_bar.dart';
 import 'package:dictionary_app/widgets/list_expression.dart';
 
 import 'package:dictionary_app/widgets/my_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,10 +17,26 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Expression> displayedExpressions = expressionData;
+  List<Expression> displayedExpressions = [];
+  List<Expression> allExpressions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadJsonData();
+  }
+
+  Future<void> loadJsonData() async {
+    final String response = await rootBundle.loadString('assets/data/expressions.json');
+    final data = await json.decode(response) as List;
+    setState(() {
+      allExpressions = data.map((json) => Expression.fromJson(json)).toList();
+      displayedExpressions = allExpressions;
+    });
+  }
 
   void _filterExpressions(String query) {
-    final filtered = expressionData.where((expression) {
+    final filtered = allExpressions.where((expression) {
       final expressionLower = expression.expressionName.toLowerCase();
       final queryLower = query.toLowerCase();
       return expressionLower.contains(queryLower);
